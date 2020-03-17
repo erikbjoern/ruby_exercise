f4834b1645aa16453b8cb80f579d904df9f272d4
Erik Björn <erik.g.bjoern@gmail.com>
2020-03-17 17:13:14 +0100

    creates my_group.rb adds persons to array my_group

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+person_1 = {name: "Erik", age: 24, gender: "male"} 
+person_2 = {name: "Anders", age: 28, gender: "male"}
+person_3 = {name: "Johan", age: 33, gender: "male"}
+
+my_group = [person_1, person_2, person_3]
